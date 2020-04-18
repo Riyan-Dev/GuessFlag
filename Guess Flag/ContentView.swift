5ff513813d050b6fb4de28613d468dee6f222e53
Muhammad Riyan <riyan20riyan@icloud.com>
2020-04-18 20:05:31 +0500

    Initial Commit

---
+++ Guess Flag/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guess Flag
+//
+//  Created by Muhammad Riyan on 18/04/2020.
+//  Copyright © 2020 Muhammad Riyan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
